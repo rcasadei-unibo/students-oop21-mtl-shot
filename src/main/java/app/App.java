cc2bed411d720a84957f7815e6df6ed5ab323fe1
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2022-04-12 19:16:55 +0200

    Build setup

---
+++ src/main/java/app/App.java
@@ -0,0 +1,13 @@
+package app;
+
+import javafx.application.Application;
+
+/** Main application entry-point. */
+
+public final class App {
+    private App() { }
+
+    public static void main(final String[] args) {
+        Application.launch(MetalShot.class, args);
+    }
+}
--- src/main/java/app/MetalShot.java
+++ src/main/java/app/MetalShot.java
@@ -1,4 +1,4 @@
-package it.unibo.samplejavafx;
+package app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -6,14 +6,14 @@ import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class JavaFXApp2 extends Application {
+public final class MetalShot extends Application {
 
     @Override
-    public final void start(final Stage primaryStage) throws Exception {
-        final Label message = new Label("Hello again JavaFX!"); 
+    public void start(final Stage primaryStage) throws Exception {
+        final Label message = new Label("Hello, JavaFX!"); 
         message.setFont(new Font(100));
         primaryStage.setScene(new Scene(message));
-        primaryStage.setTitle("JavaFXApp2");
+        primaryStage.setTitle("Hello");
         primaryStage.show();
     }
 
@@ -26,8 +26,8 @@ public class JavaFXApp2 extends Application {
             // the constructor will never be called directly.
         }
 
-        public static void main(final String... args) {
-            JavaFXApp2.run(args);
+        public static void main(final String...args) {
+            Application.launch(MetalShot.class, args);
         }
     }
 }
