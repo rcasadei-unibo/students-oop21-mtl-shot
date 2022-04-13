7005ca52a89f4f091a73c5d0766688940eecac81
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2022-04-13 18:53:06 +0200

    start implementing PlayerTest

--- src/test/java/playertest/PlayerTest.java
+++ src/test/java/playertest/PlayerTest.java
@@ -3,12 +3,22 @@ package playertest;
 import org.junit.jupiter.api.Test;
 
 import model.character.Player;
+import model.character.Player.PlayerBuilder;
+import model.character.tools.health.SimpleHealth;
+import utilities.Vector;
 
 public class PlayerTest {
 	
-	//private final Player player;
+	private final Player player;
 	
-	private PlayerTest() {
+	public PlayerTest() {
+		player = new PlayerBuilder()
+				.health(new SimpleHealth())
+				.hitbox(new Vector(1, 2))
+				.lives(3)
+				.position(new Vector(0,0))
+				.build();
+		System.out.println("tutto ok");
 	}
 	
     @Test 
