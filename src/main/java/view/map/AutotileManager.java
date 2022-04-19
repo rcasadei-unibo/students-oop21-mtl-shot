7a00f575b45b3274071bfb47db20aaa1811ca500
TommasoTurci <tommaso.turci5@studio.unibo.it>
2022-04-19 13:42:06 +0200

    Start implementing AutotileManager

--- src/main/java/view/map/AutotileManager.java
+++ src/main/java/view/map/AutotileManager.java
@@ -1,8 +1,11 @@
 package view.map;
 
-import java.awt.Image;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
 import model.map.tile.Tile;
 
@@ -11,12 +14,30 @@ public class AutotileManager {
     private List<Tile> tileList;
     private Image tileset;
     
-    public AutotileManager(List<Tile> tileList, Image tileset) {
+    public AutotileManager(List<Tile> tileList, Image tileset) throws FileNotFoundException {
+	PixelReader reader = new Image(new FileInputStream("src\\\\main\\\\resources\\\\DesertTileSet.png")).getPixelReader();
 	this.tileList = tileList;
 	this.tileset = tileset;
     }
     
     public WritableImage autotile() {
+	/*BufferedImage image = ImageIO.read(new File(path, "image.png"));
+	BufferedImage overlay = ImageIO.read(new File(path, "overlay.png"));
+
+	// create the new image, canvas size is the max. of both image sizes
+	int w = Math.max(image.getWidth(), overlay.getWidth());
+	int h = Math.max(image.getHeight(), overlay.getHeight());
+	BufferedImage combined = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+
+	// paint both images, preserving the alpha channels
+	Graphics g = combined.getGraphics();
+	g.drawImage(image, 0, 0, null);
+	g.drawImage(overlay, 0, 0, null);
+
+	g.dispose();
+
+	// Save as new image
+	ImageIO.write(combined, "PNG", new File(path, "combined.png"));*/
 	return null;
     }
     
