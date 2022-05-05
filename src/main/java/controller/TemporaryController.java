dd0c80471f7c383a483aad425285c5275b7fd145
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2022-05-05 10:21:50 +0200

    Instantiate player and dummy in TemporaryController (for tests)

--- src/main/java/controller/TemporaryController.java
+++ src/main/java/controller/TemporaryController.java
@@ -7,15 +7,35 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 import javafx.util.Duration;
+import model.character.Player;
+import model.character.tools.health.SimpleHealth;
+import util.Vector;
 
 public class TemporaryController {
 	private Timeline gameLoop;
 	
 	//Instance of model (Stage?)
+	private Player player;
+	private Player dummy;
+	
 	private final MetalShot viewReference;
 	private BulletsController bulletsController;
 	
 	public TemporaryController(final MetalShot viewReference) {
+		var pb = new Player.PlayerBuilder();
+		this.player = pb.position(new Vector(0, 0))
+				.health(new SimpleHealth())
+				.lives(1)
+				.hitbox(new Vector(32, 32))
+				.build();
+		
+		var pb2 = new Player.PlayerBuilder();
+		this.dummy = pb2.position(new Vector(0, 0))
+				.health(new SimpleHealth())
+				.lives(1)
+				.hitbox(new Vector(32, 32))
+				.build();
+		
 		this.viewReference = viewReference;
 		this.gameLoop = new Timeline(
 				new KeyFrame(Duration.seconds(0.01), new EventHandler<ActionEvent>() {
@@ -64,4 +84,6 @@ public class TemporaryController {
 	public void nextLevel() {
 		// TODO
 	}
+	
+	
 }
