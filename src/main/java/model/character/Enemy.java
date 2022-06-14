b49502af6b3ae412fe3a16810e0a9f51251fb6a4
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2022-06-14 12:50:59 +0200

    Minor changes

--- src/main/java/model/character/Enemy.java
+++ src/main/java/model/character/Enemy.java
@@ -7,7 +7,7 @@ import util.Vector2D;
  * TODO: javadoc.
  *
  */
-public class Enemy extends Character{
+public class Enemy extends Character {
 	
 	public Enemy(final Vector2D position, final Vector2D hitbox, final Health health) {
 		super(position, hitbox, health, new P2020());
