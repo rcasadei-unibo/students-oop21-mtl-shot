3c5617ef2cedef9ef348708f62b0dd26742dd4b6
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2022-06-17 19:12:29 +0200

    Add javadoc in SoundsController

--- src/main/java/controller/SoundsController.java
+++ src/main/java/controller/SoundsController.java
@@ -6,6 +6,11 @@ import java.util.Map;
 import view.sounds.SoundManager;
 import view.sounds.SoundManager.Sounds;
 
+/**
+ * It handles sounds putting a cllodown
+ * between two sounds of the same type.
+ *
+ */
 public class SoundsController {
 	private Map<Sounds, Cooldown> timers;
 	private SoundManager soundManager;
@@ -14,7 +19,10 @@ public class SoundsController {
 		this.timers = new HashMap<>();
 		this.soundManager = new SoundManager();
 	}
-
+	
+	/**
+	 * This function is called by Controller every game tick.
+	 */
 	public void controllerTick() {
 		this.timers.forEach((c, sc) -> { sc.tick(); });
 		this.timers.entrySet().removeIf(e -> e.getValue().isCooldownOver());
@@ -22,6 +30,12 @@ public class SoundsController {
 	
 	/* Example sound playing: this.soundsController.tryToPlaySound(Sounds.BULLET_FIRING) */
 	
+	/**
+	 * Plays a sound only if the cooldown corresponding to
+	 * its soundType is over.
+	 * @param soundType: the type of the sound to be played.
+	 * @return true if the sound has been played, otherwise false.
+	 */
 	public boolean tryToPlaySound(final Sounds soundType) {
 		if (!this.timers.containsKey(soundType)) {
 			/* Se un suono di tipo soundType non è stato fatto ancora partire, lo fa partire TODO: translate comment */
