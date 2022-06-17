2da19e5d7da3c1e00d68053fd308a0eb60b88197
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2022-06-17 13:00:32 +0200

    Remove debug print

--- src/main/java/controller/SoundsController.java
+++ src/main/java/controller/SoundsController.java
@@ -20,9 +20,6 @@ public class SoundsController {
 	public void controllerTick() {
 		this.timers.forEach((c, sc) -> { sc.tick(); });
 		this.timers.entrySet().removeIf(e -> e.getValue().isCooldownOver());
-		/*if (!this.timers.isEmpty()) {
-			System.out.println(this.timers.toString());
-		}*/
 	}
 	
 	/* Example sound playing: this.soundsController.tryToPlaySound(Sounds.BULLET_FIRING) */
@@ -31,8 +28,7 @@ public class SoundsController {
 		if (!this.timers.containsKey(soundType)) {
 			/* Se un suono di tipo soundType non è stato fatto ancora partire, lo fa partire TODO: translate comment */
 			this.timers.put(soundType, new Cooldown(100));	/* Temporal gap between two sounds of the same type (TODO: too high, fix) */
-			//this.soundManager.playSound(soundType);
-			System.out.println("Play sound " + soundType.name() + " guh");
+			this.soundManager.playSound(soundType);
 			return true;
 		}
 		return false;
