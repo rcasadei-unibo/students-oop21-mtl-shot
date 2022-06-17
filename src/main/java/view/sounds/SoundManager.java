3525deb08c7dae96dbadfd8f4e86767768ebd845
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2022-06-17 19:00:00 +0200

    Add javadoc in SoundManager

--- src/main/java/view/sounds/SoundManager.java
+++ src/main/java/view/sounds/SoundManager.java
@@ -5,7 +5,12 @@ import javafx.scene.media.AudioClip;
 import java.nio.file.Path;
 
 /**
- * TODO: write javadoc
+ * It contains every sound. Each one
+ * is loaded the first time it is played
+ * It also Contains public enum Sounds,
+ * used to identify and play a specific
+ * sound. The only callable method is
+ * playSound(Sounds).
  *
  */
 public class SoundManager {
@@ -13,12 +18,16 @@ public class SoundManager {
 		
 	}
 	
+	/**
+	 * Use this method to play a sound.
+	 * @param sound to be played.
+	 */
 	public void playSound(final Sounds sound) {
 		sound.getAudioClip().play();
 	}
 	
 	/**
-	 * TODO: write javadoc
+	 * It contains, identifies and loads every sound.
 	 *
 	 */
 	public enum Sounds {
@@ -38,6 +47,9 @@ public class SoundManager {
 			this.audioClip = audioClip;
 		}
 		
+		/**
+		 * @return the audioClip object associated with "this" sound.
+		 */
 		public AudioClip getAudioClip() {
 			return this.audioClip;
 		}
